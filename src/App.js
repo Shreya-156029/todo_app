1fee626d9ddc6e9b1715bedeea4efb8da8fd956c
SHREYA <skulkarni156029@gmail.com>
2020-10-03 15:09:54 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Note from './components/Note'; 
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+       noteText:'',
+       notes:[],
+    }
+  }
+  updateNoteText(noteText){
+            this.setState({
+              noteText : noteText.target.value
+            })
+            console.log(this.state.noteext)
+  }
+  handleKeyPress=(event)=>{
+          if(event.key === 'Enter')
+          {
+ let notesarr=this.state.notes;
+    notesarr.push(this.state.noteText);
+    this.setState({
+      noteText:''
+    });
+          }
+  }
+  deleteNote(index)
+  {
+    let notesarr=this.state.notes;
+    notesarr.splice(index,1);
+    this.setState({
+      notes:notesarr
+    })
+  }
+  AddNote()
+  {
+    if(this.state.noteText===' ')
+    {return}
+
+    let notesarr=this.state.notes;
+    notesarr.push(this.state.noteText);
+    this.setState({
+      noteText:''
+    });
+    this.textInput.focus();
+
+  }
+render(){
+
+  let notes=this.state.notes.map((val,key)=>{
+             
+             return <Note key={key} text={val}
+             deleteMethod={ ()=> this.deleteNote(key)  }
+            />
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cotainer">
+     <div className="header">React Todo App</div>
+     {notes}
+     <div className="btn" onClick={this.AddNote.bind(this)}>+</div> 
+     <input type="text" 
+     ref={((input) => {this.textInput = input })} 
+     className="textInput" 
+     value={this.state.noteText}
+     onChange={ noteText => this.updateNoteText(noteText) }
+     onKeyPress ={this.handleKeyPress.bind(this)}  
+     />
     </div>
+    
   );
+}
 }
 
 export default App;
